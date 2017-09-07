bb1d5fdd58c531106b46b15969deedf818cfd10a
Juan Caicedo <jjcc1421@gmail.com>
2017-09-07 00:27:59 -0500

    First react class

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {test} from './test/test.jsx';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
+    return <test></test>;
   }
 }
 
---
+++ src/client/app/test/test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Test extends React.Component {
+  render () {
+    return <p> Hello React!</p>;
+  }
+}
+
+export default Test;
